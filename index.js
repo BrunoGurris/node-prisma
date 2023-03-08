b2bac781c1c131b1946b0260da281202dcbc5c21
BrunoGurris <brunoagpires@gmail.com>
2023-03-07 22:47:16 -0300

    feat(prisma): added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const routerUsers = require('./src/routes/users.router')
+const usersRouter = require('./src/routes/users.router')
 
-app.use('/users', routerUsers)
+app.use('/users', usersRouter)
+
 app.use(express.json())
 app.listen(9000)
